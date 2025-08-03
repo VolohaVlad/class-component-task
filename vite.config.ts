aa1506b559a3147e4336e279b5a89b97b95775aa
VolohaVlad <volohanovich@mail.ru>
2025-08-03 22:31:11 +0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/class-component-task/',
+  base: '/class-component-task',
   plugins: [react(), tailwindcss()],
 });
